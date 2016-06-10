de3b8a6c51a40a4644cd79c5041551a1e49e192c
franschristian <frans.christian@INDODC-CP7PEAU.dir.svc.accenture.com>
2016-06-10 18:04:28 +0700

    add retailers frequency page

--- src/admin/app/retailers/components/retailer-frequency.component.ts
+++ src/admin/app/retailers/components/retailer-frequency.component.ts
@@ -1,5 +1,5 @@
 import {Component} from 'angular2/core';
-import {Router, ROUTER_DIRECTIVES, RouteConfig} from 'angular2/router';
+import {Router, ROUTER_DIRECTIVES} from 'angular2/router';
 import {Layout} from '../../../models/layout';
 import {LayoutService} from '../../shared/services/layout.service';
 import {PageNavigationService} from '../../shared/services/page-navigation.service';
@@ -7,12 +7,10 @@ import {MatchMediaService} from '../../shared/services/match-media.service';
 import {HeaderService} from '../../shared/services/header.service';
 import {NgModel} from 'angular2/common';
 import {FilterComponent} from '../../shared/components/filter.component';
-import {RetailerDetailComponent} from './retailer-detail.component';
-import {RetailerFrequencyComponent} from './retailer-frequency.component';
 
 @Component({
-    selector: 'layout-retailer',
-    templateUrl: './app/retailers/components/layout-retailer.component.html',
+    selector: 'retailer-frequency',
+    templateUrl: './app/retailers/components/retailer-frequency.component.html',
     // templateUrl: 'app/shared/components/home.component.html',
     directives: [
         NgModel,
@@ -21,21 +19,7 @@ import {RetailerFrequencyComponent} from './retailer-frequency.component';
     ],
 })
 
-@RouteConfig([
-    {
-        path: '/retailerDetail',
-        name: 'RetailerDetail',
-        component: RetailerDetailComponent,
-        useAsDefault: true
-    },
-    {
-        path: '/retailerFrequency',
-        name: 'RetailerFrequency',
-        component: RetailerFrequencyComponent
-    }
-])
-
-export class LayoutRetailerComponent {
+export class RetailerFrequencyComponent {
     private vDate: Date;
     
     constructor(
@@ -45,22 +29,16 @@ export class LayoutRetailerComponent {
         private _pageNavigationService: PageNavigationService,
         private _headerService: HeaderService
     ) {
-        window.scrollTo(0,0);
-        this._layoutService.setCurrentPage('UsersDetail');
+        this._layoutService.setCurrentPage('RetailerFrequency');
         this._headerService.setTitle('iDSP Administration Panel');
-        // this._headerService.setNavigationState('Products');
         this.vDate = new Date();
     }
     
     getToday() {
         return this.vDate.toLocaleDateString('en-US');
     }
-    
+
     getResize() {
         return this._matchMediaService.getMm();
-    }
-
-    goTo(page:string) {
-        this._pageNavigationService.navigate(page, null, null);
     }
 }
