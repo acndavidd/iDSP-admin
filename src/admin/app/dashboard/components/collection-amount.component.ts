99b43560ec9eaff3b12c9fac2af8053881fd9f8c
franschristian <frans.christian@INDODC-CP7PEAU.dir.svc.accenture.com>
2016-06-15 15:48:20 +0700

    finalize sorting component and apply the sort function in sales order and collection amount page

--- src/admin/app/dashboard/components/collection-amount.component.ts
+++ src/admin/app/dashboard/components/collection-amount.component.ts
@@ -7,16 +7,21 @@ import {MatchMediaService} from '../../shared/services/match-media.service';
 import {HeaderService} from '../../shared/services/header.service';
 import {NgModel} from 'angular2/common';
 import {FilterComponent} from '../../shared/components/filter.component';
+import {SortService} from '../../shared/services/sort.service';
+import {DashboardService} from '../services/dashboard.service';
 
 @Component({
     selector: 'collection-amount',
     templateUrl: './app/dashboard/components/collection-amount.component.html',
-    // templateUrl: 'app/shared/components/home.component.html',
     directives: [
         NgModel,
         ROUTER_DIRECTIVES,
         FilterComponent
     ],
+    providers: [
+        SortService,
+        DashboardService
+    ]
 })
 
 export class CollectionAmountComponent {
@@ -27,7 +32,9 @@ export class CollectionAmountComponent {
         private _layoutService: LayoutService,
         private _matchMediaService: MatchMediaService,
         private _pageNavigationService: PageNavigationService,
-        private _headerService: HeaderService
+        private _headerService: HeaderService,
+        private _sortService: SortService,
+        private _dashboardService: DashboardService
     ) {
         window.scrollTo(0,0);
         this._layoutService.setCurrentPage('CollectionAmount');
@@ -53,5 +60,21 @@ export class CollectionAmountComponent {
 
     goToRetailerDetail(orderNo:string) {
         this._pageNavigationService.navigate('Retailers/RetailerDetail', null, null);
+    }
+
+    sort(header:string) {
+        this._sortService.sort(this.getCollectionAmountList(), header);
+    }
+
+    getSortStatus(header:string) {
+        if(this._sortService.sortby === undefined) {
+            return false;
+        }else {
+            return this._sortService.sortby[header]; 
+        }
+    }
+
+    getCollectionAmountList() {
+        return this._dashboardService.getCollectionAmountList();
     }
 }
